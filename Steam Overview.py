d088fc4022b36974808b30fd5dcebaac48cda9df
Jack Neil Dobson <jackndobson@gmail.com>
2017-06-01 23:04:23 +0100

    Fix whitespace
    I'm a stickler for those 4 spacios.

--- Steam Overview.py
+++ Steam Overview.py
@@ -85,7 +85,7 @@ class Game:
         self.name = info.get('name', self.directory)
         self.steamLastUpdate = info.get('LastUpdated', 0)
         self.size = int(info.get('SizeOnDisk', 0))
-
+    
         if self.directory:
             gamepath = file.path(os.path.split(path)[0], 'common', self.directory)
             self.size = file.dirsize(gamepath)[0]
@@ -94,29 +94,29 @@ class Game:
     
     def toDictionary(self):
         return {key: getattr(self, key, None) for key in self.__slots__}
-
+    
     def __repr__(self):
         return '<Game, {}>'.format(repr(self.toDictionary()).replace('{','').replace('}',''))
 
 class Library(list):
     __slots__ = ['path', 'sizeTotal', 'sizeUsed', 'sizeGames']
-
+    
     def __init__(self, path):
         '''List of games, given path to library (NOT /steamapps).'''
         self.path = file.path(path)
-
+        
         gamespath = file.path(path, 'steamapps')
         for i in os.listdir(gamespath):
             if i.startswith('appmanifest_') and i.endswith('.acf'):
                 i = file.path(gamespath, i)
                 self.append(Game(i))
-
+        
         self.sizeTotal, self.sizeUsed, _ = shutil.disk_usage(path)
         self.sizeGames = sum(game.size for game in self)
-
+    
     def __repr__(self):
         return '<Library, {} game{}>'.format(len(self), 's' if len(self) != 1 else '')
-
+    
     def toDictionary(self):
         return {'path': self.path, 'sizeTotal': self.sizeTotal, 'sizeUsed': self.sizeUsed,
                 'sizeGames': self.sizeGames, 'games': [game.toDictionary() for game in self]}
@@ -138,6 +138,7 @@ def main():
         logtxt.append(text)
     
     log('Getting paths...')
+    
     libraries = []
     def getLibraries(paths):
         for path in paths:
@@ -166,12 +167,12 @@ def main():
         getLibraries(getLibraryPaths(base))
     
     log('Done, dumping to `libraries.js`…')
-
+    
     with open('libraries.js', 'w') as f:
         f.write(FORMAT.format(
             '\n * '.join(logtxt), time.time(),
             json.dumps(libraries, indent=1)))
-
+    
     log('Opening `viewer/viewer.html`…')
     viewer = file.path(os.getcwd(), 'viewer', 'viewer.html')
     webbrowser.open_new_tab(viewer)
