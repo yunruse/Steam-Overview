7c8d77ac54961cc295f3fa081ecba5b713505af7
Jack Neil Dobson <jackndobson@gmail.com>
2017-06-12 19:07:44 +0100

    Add game-item pseudo-hover from bar mouseover
    Look, I'm really bored, and I should be revising, but I'll be damned if
    I don't make this one little feature.

--- viewer/js/gameSelector.js
+++ viewer/js/gameSelector.js
@@ -10,6 +10,10 @@ function bindGame(game){
   game.element.onmouseover = function(){ gameSelect(game, true, false)};
   game.element.onmouseout = function(){ gameSelect(game, false, false)};
   game.element.onclick = function(){ gameSelect(game, true, true)};
+  
+  game.barElement.onmouseover = function(){ gameSelect(game, true, false)};
+  game.barElement.onmouseout = function(){ gameSelect(game, false, false)};
+  game.barElement.onclick = function(){ gameSelect(game, true, true)};
 }
 
 function gameSelect(game, didMouse, didClick){
@@ -38,11 +42,13 @@ function gameSelect(game, didMouse, didClick){
 function gameHighlight(game, doHighlight, doLockIn, doDisplayPotential) {
   try { game.barElement.classList }
   catch(e) { console.log(game) }
+  
   var bC = game.barElement.classList;
   doHighlight ? bC.add('hovered') : bC.remove('hovered');
   doLockIn ? bC.add('locked') : bC.remove('locked')
   
   var gC = game.element.classList;
+  doHighlight ? gC.add('hovered') : gC.remove('hovered');
   doLockIn ? gC.add('locked') : gC.remove('locked')
   
   if( !doDisplayPotential ){ return; }
