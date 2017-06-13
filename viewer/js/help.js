c3e0b397846ccc9adb0652555048cf00e4d87224
Jack Neil Dobson <jackndobson@gmail.com>
2017-06-13 17:14:31 +0100

    Move tutorial to help.js

---
+++ viewer/js/help.js
@@ -0,0 +1,56 @@
+/* Automatic (and manual) tutorial */
+
+var hintInterfaceState;
+
+startTutorial = function(){
+  hintInterfaceState = 1;
+  hintInterfaceLoop();
+}
+
+hintInterfaceLoop = function(){
+  if( gameBindingsLength === 0 ){
+    return;
+  }
+  var hintBindingID = Math.min(3, Math.floor(gameBindingsLength / 2))
+      game = gameBindings[hintBindingID];
+  
+  var timeToWait = hintInterfaceDisplay(game, hintInterfaceState);
+  // carries out and returns time to wait
+  if( timeToWait === "stop" ){ return; }
+  setTimeout(hintInterfaceLoop, timeToWait);
+  hintInterfaceState++;
+}
+
+hintInterfaceDisplay = function(game, state){
+  var lC = game.element.classList,
+      bC = game.barElement.classList,
+      pL = game.element.playLink;
+  
+  switch( state ){
+    case 3:
+      lC.remove('locked');
+      bC.remove('locked');
+      pL.classList.add('smaller');
+      pL.innerText = "[Games will display their potential size on other libraries]";
+      return 5000;
+    case 2:
+      lC.add('locked');
+      bC.add('locked');
+      pL.innerText = "[Click to lock in]";
+      return 5000;
+    case 1:
+      lC.add('hovered');
+      bC.add('hovered');
+      pL.innerText = "[Mouseover to see info]";
+      return 3000;
+    default:
+      lC.remove('locked');
+      bC.remove('locked');
+      lC.remove('hovered');
+      bC.remove('hovered');
+      pL.classList.remove('smaller');
+      tutorialBox.classList.add('visible');
+      pL.innerText = "Play..."
+      return "stop";
+  }
+}
