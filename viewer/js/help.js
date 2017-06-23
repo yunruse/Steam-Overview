b40eb1fb32d3513f2185d8c9af2d8c162f75cce7
Jack Neil Dobson <jackndobson@gmail.com>
2017-06-23 18:39:14 +0100

    Tighten tutorial control

--- viewer/js/help.js
+++ viewer/js/help.js
@@ -6,59 +6,73 @@ toggle = function(item, caller) {
 
 /* Automatic (and manual) tutorial */
 
-var hintInterfaceState, hintInterfaceOthers;
+var tutorialTimeStarted;
 
-startTutorial = function(){
-  if( gameBindings.length === 0 ){ return; }
+setupTutorial = function(){
+  if( gameBindings.length === 0 ){ tutorialBox.classList.add('hidden'); }
+  var hintBindingID = Math.min(3, Math.floor(gameBindings.length / 2))
+  gameBindings.tutorialShown = gameBindings[hintBindingID]
   
-  var hintBindingID = Math.min(3, Math.floor(gameBindings.length / 2));
-  gameBindings.tutorialShown = gameBindings[hintBindingID];
-  
-  otherGames = [];
+  otherGames = []
   for( var i = 0; i < gameBindings.length; i++ ){
-    if( i == hintBindingID ){ continue; }
-    otherGames.push(gameBindings[i].element);
+    if( i == hintBindingID ){ continue }
+    otherGames.push(gameBindings[i].element)
   }
-  gameBindings.tutorialShadowElements = otherGames;
+  gameBindings.tutorialShadowElements = otherGames
   
-  hintInterfaceState = 1;
-  hintInterfaceLoop();
+  tutorialTimeStarted = 0
 }
 
-hintInterfaceLoop = function(){  
-  var timeToWait = hintInterfaceDisplay(hintInterfaceState);
-  // carries out and returns time to wait
+startTutorial = function(){  
+  if( tutorialTimeStarted !== 0 ){
+    hintInterfaceDisplay(4, tutorialTimeStarted)
+    tutorialTimeStarted = 0
+    return
+  }
   
-  if( timeToWait === "stop" ){ return; }
-  setTimeout(hintInterfaceLoop, timeToWait);
-  hintInterfaceState++;
+  var timeStarted = (new Date).valueOf();
+  tutorialTimeStarted = timeStarted;
+  
+  hintInterfaceDisplay(1, timeStarted)
+  setTimeout(function() {hintInterfaceDisplay(2, timeStarted)},  3000)
+  setTimeout(function() {hintInterfaceDisplay(3, timeStarted)},  8000)
+  setTimeout(function() {hintInterfaceDisplay(4, timeStarted)}, 13000)
 }
 
-hintInterfaceDisplay = function(state){
+hintInterfaceDisplay = function(state, timeStarted){
+  console.log(state, timeStarted % 1000, tutorialTimeStarted % 1000)
+  if( tutorialTimeStarted !== timeStarted ){ return; } // manually stopped
+  
   var game = gameBindings.tutorialShown,
       others = gameBindings.tutorialShadowElements,
-      pL = game.element.playLink;
+      pL = game.element.playLink
   
-  // When in tutorial, toggle certain states
-  classBool(state < 4, 'shadowed', ...others)
+  // Shadow non-tutorial game
+  for (var i = 0; i < others.length; i++ ){
+    classBool(state < 4, 'shadowed', others[i])
+  }
+  // Make 'play link' smaller for instructional purposes
   classBool(state < 4, 'smaller', game.element.playLink)
-  classBool(state < 4, 'hidden', tutorialBox)
+  classBool(state < 4, 'hovered', tutorialBox)
   
+  //               [do hover]   [toggle lock]
+  gameSelect(game, state < 4, 2 <= state <= 3)
+  // Force styles (just in case)
   classBool(state == 2, 'locked', game.element, game.barElement)
   classBool(state < 4, 'hovered', game.element, game.barElement)
   
   switch( state ){
     case 1:
-      pL.innerText = "[Mouseover to see info]";
-      return 3000;
+      pL.innerText = "[Mouseover to see info]"
+      break
     case 2:
-      pL.innerText = "[Click to lock in]";
-      return 5000;
+      pL.innerText = "[Click to lock in]"
+      break
     case 3:
-      pL.innerText = "[Scroll down to see size on other libraries]";
-      return 5000;
-    case 4:
+      pL.innerText = "[Scroll down to see size on other libraries]"
+      break
+    default:
+      isTutorialRunning = false
       pL.innerText = "Play..."
-      return "stop";
   }
 }
--- viewer/js/interface.js
+++ viewer/js/interface.js
@@ -32,4 +32,5 @@ window.onload = function(){
     throw e;
   }
   displayTimeSince();
+  setupTutorial()
 }
