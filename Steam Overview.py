d6586aef3ebfb988f63473f313144c16e0ecc790
Mia yun Ruse <mia@yunru.se>
2021-11-19 21:57:13 +0000

    Whitespace fixes

--- Steam Overview.py
+++ Steam Overview.py
@@ -20,9 +20,10 @@ ESTIMATE_THRESHOLD_MiB = 256
 
 # Watch out, below here be dragons.
 
+
 def _getPaths(log):
-    logPath = lambda path: log("Trying '{}'…", path)
-    
+    def logPath(path): return log("Trying '{}'…", path)
+
     paths = getLibraryPaths(None, logPath)
     attempts = 0
     while len(paths) == 0:
@@ -30,13 +31,15 @@ def _getPaths(log):
             log("Couldn't automatically find Steam install directory.")
             print("Sorry about that. Please provide a path:")
         elif attempts == 1:
-            print("Still couldn't find anything, sorry. The install directory should contain")
+            print(
+                "Still couldn't find anything, sorry. The install directory should contain")
             print("the Steam program alongside 'steamapps/libraryfolders.vdf'.)")
-        
+
         attempts += 1
         paths = getLibraryPaths(input("~ "), logPath)
-    
+
     return paths
+
 
 FORMAT = '''\
 /* Steam Overview %s */
@@ -45,20 +48,25 @@ var lastRetrieved = {},
     libraries = {};''' % __ver__
 
 # Used to JSONify Game and Drive objects
-slottableToDict = lambda obj: {key: getattr(obj, key) for key in obj.__slots__}
+
+
+def slottableToDict(obj): return {key: getattr(obj, key)
+                                  for key in obj.__slots__}
+
 
 def _main(log):
     log('''STEAM OVERVIEW VERSION {}\n''', __ver__)
-    
+
     paths = _getPaths(log)
-    
+
     if not paths:
         log('No steam install found!')
         return
-    
+
     # Get steam-installed games
-    
+
     drives = []
+
     def getDrive(path):
         '''Get drive at provided path, adding it to drives if nonexistent.'''
         base = path.drive or path.root
@@ -69,44 +77,44 @@ def _main(log):
             drv = Drive(base)
             drives.append(drv)
         return drv
-    
+
     for path in paths:
         log("Searching '{}'… ", path)
         path = Path(path)
         drv = getDrive(path)
         drv.appendLibrary(path)
-    
+
     log("Searching user shortcuts…")
-    
+
     userdata = Path(paths[0]) / 'userdata'
     shortcutsExist = False
-    
+
     for userID in os.listdir(userdata):
         conf = userdata / userID / 'config' / 'shortcuts.vdf'
         if os.path.isfile(conf):
             for game in shortcutGames(conf):
                 shortcutsExist = True
-                # To ensure shortcut 
+                # To ensure shortcut
                 path = Path(game.ID)
                 drv = getDrive(path)
                 drv.games.append(game)
-    
+
     log("\nGetting game sizes...")
     if shortcutsExist:
         log("Games marked * are shortcuts and must have their size calculated.")
     if ESTIMATE_THRESHOLD_MiB:
         log("Games marked ~ have a size below {} MiB, and use Steam's estimate.",
             ESTIMATE_THRESHOLD_MiB)
-    
+
     for drv in drives:
         log()
         log('{} has {} games:', drv.path, len(drv.games))
         drv.games.sort(key=lambda game: game.sizeEstimate, reverse=True)
-        
+
         for count, game in enumerate(drv.games, start=1):
             e = game.sizeEstimate
             symbol = ''
-            
+
             if e == 0:
                 # steam shortcut
                 symbol = '*'
@@ -114,40 +122,43 @@ def _main(log):
                 # auto-skip
                 symbol = '~'
                 game.size = e
-            
+
             game.getSize()
-            
+
             log('{:>10} {:1} {}',
                 bytesize(game.size, digits=1, binary=True),
                 symbol, game.name)
-        
+
         # get drive size info
         drv.getSize()
-    
+
     # sort drives by their size - typically, a smaller drive will
     # be an SSD and so the 'primary' drive
     drives.sort(key=lambda l: l.sizeTotal)
-    
+
     log('\nPassing to `viewer/viewer.html`…')
-    
+
     with open('libraries.js', 'w') as f:
         _json = json.dumps(drives, indent=1, default=slottableToDict)
         f.write(FORMAT.format(time.time(), _json))
-    
+
     viewer = Path(os.getcwd()) / 'viewer' / 'viewer.html'
     webbrowser.open_new_tab(viewer)
 
+
 class Logger:
     __slots__ = 'files'.split()
+
     def __init__(self, *files):
         if not files:
             files = (sys.stdout, )
         self.files = files
-    
+
     def log(self, text='', *args, **kwargs):
         text = text.format(*args, **kwargs)
         for f in self.files:
             print(text, file=f)
+
 
 if __name__ == '__main__':
     with open('log.txt', 'w', encoding='utf8') as f:
--- steamfile.py
+++ steamfile.py
@@ -14,16 +14,18 @@ __dir__ = (
     'Game', 'Library')
 
 size = collections.namedtuple('size', 'totalSize fileCount')
+
+
 def dirsize(path):
     '''Get bytes used by directory and amount of files.
-    
+
     > dirsize('/path/to/directory/')
     size(134240, 27)
     > dirsize('/path/to/file.txt')
     size(5836, 1)
     > dirsize('notanactualfile')
     size(0, 0)'''
-        
+
     if not os.path.exists(path):
         return size(0, 0)
     elif os.path.isfile(path):
@@ -31,45 +33,49 @@ def dirsize(path):
             return size(os.lstat(path).st_size, 1)
         except os.error:
             return size(0, 1)
-    
+
     totalSize = 0
     seen = set()
-    
+
     for dirpath, dirs, files in os.walk(path):
         for file in files:
             file = os.path.join(dirpath, file)
-            
+
             try:
                 stat = os.lstat(file)
             except os.error:
                 continue
-            
+
             if stat.st_ino in seen:
                 continue
             else:
                 seen.add(stat.st_ino)
-            
+
             totalSize += stat.st_size
-    
+
     return size(totalSize, len(seen))
+
 
 def bytesize(size, digits=1, binary=False):
     divisor = 1024 if binary else 1000
     for prefix in " KMGTPEZY":
-        if prefix == ' ': prefix = ''
+        if prefix == ' ':
+            prefix = ''
         if abs(size) < divisor:
             break
         elif prefix != "Y":
             size /= divisor
     return "{} {}{}B".format(round(size, digits), prefix, "i"*bool(prefix and binary))
 
+
 STEAMENTRY = re.compile(r'^\t"(.+?)"\t\t"(.+?)"')
+
 
 def readSteamFile(path):
     '''Scrapes top-level information from Steam-formatted ACF or VDF file as dictionary.
-    
+
     Any numerical indexes are placed into dict['_list'].'''
-    
+
     info = {'_list': []}
     path = Path(path)
     with path.open(encoding='utf8') as file:
@@ -84,22 +90,24 @@ def readSteamFile(path):
                 info[key] = value
     return info
 
+
 def isSteamBase(path):
     '''Return True if the path is the base Steam library, containing /steamapps/libraryfolders.vdf'''
-    
+
     return (Path(path) / 'steamapps' / 'libraryfolders.vdf').is_file()
+
 
 def isSteamLibrary(path):
     '''Return True if the path is a valid Steam library, containing /steamapps/common/'''
-    
+
     return isSteamBase(path) or (Path(path) / 'steamapps' / 'common').is_dir()
 
 #
 # extremely OS-dependant steamBaseFinder()
-# 
+#
+
 
 if os.name == 'nt':
-    
     registryPath = None
     try:
         import winreg
@@ -112,8 +120,8 @@ if os.name == 'nt':
                 # Make a little nicer
                 registryPath = Path(registryPath.title().replace('X86', 'x86'))
         except FileNotFoundError:
-             pass
-    
+            pass
+
     try:
         from ctypes import windll
     except ModuleNotFoundError:
@@ -121,18 +129,19 @@ if os.name == 'nt':
     else:
         def _doesDriveExist(letter):
             return (windll.kernel32.GetLogicalDrives() >> (ord(letter.upper()) - 65) & 1) != 0
-    
-        driveLetters = [a + ':/' for a in string.ascii_uppercase if _doesDriveExist(a)]
-    
+
+        driveLetters = [
+            a + ':/' for a in string.ascii_uppercase if _doesDriveExist(a)]
+
     def steamBaseFinder():
         if registryPath is not None:
             yield registryPath
-        
+
         progfileKeys = ('ProgramFiles', 'ProgramFiles(x86)', 'ProgramW6432')
         for i in progfileKeys:
             if i in os.environ:
                 yield Path(os.environ[i]) / 'Steam'
-        
+
         for a in driveLetters:
             a = Path(a)
             yield a / 'Program Files (x86)' / 'Steam'
@@ -141,13 +150,15 @@ if os.name == 'nt':
 
 else:
     _UNIXBASES = (
-        '~/Library/Application Support/Steam/', #macOS
-        '~/.local/share/Steam', #Most Linux distros
-        '~/.steam/' #Some Linux distros
+        '~/Library/Application Support/Steam/',  # macOS
+        '~/.local/share/Steam',  # Most Linux distros
+        '~/.steam/'  # Some Linux distros
     )
+
     def steamBaseFinder():
         for i in _UNIXBASES:
             yield Path(i).expanduser()
+
 
 def getSteamBase(log=lambda path: None):
     for path in steamBaseFinder():
@@ -157,26 +168,29 @@ def getSteamBase(log=lambda path: None):
     else:
         return None
 
+
 def getLibraryPaths(base=None, log=lambda path: None):
     '''Returns a list of paths to Steam libraries found. If 'base' is not provided it will be found.'''
     if base is None:
         base = getSteamBase(log)
-        if base is None: #it feels wrong doing that twice
+        if base is None:  # it feels wrong doing that twice
             return []
-    
+
     try:
         vdf = readSteamFile(Path(base) / 'steamapps' / 'libraryfolders.vdf')
     except FileNotFoundError:
         return []
-    
+
     return [base] + [Path(i) for i in vdf['_list']]
+
 
 class SteamFileError(Exception):
     pass
 
+
 class Game:
     __slots__ = 'ID name installdir sizeEstimate size'.split()
-    
+
     def __init__(self, ID, name, installdir, sizeEstimate=0, size=None):
         self.ID = ID
         for i in '(™) ™ (c) (C) (r) (R) © ®'.split():
@@ -185,7 +199,7 @@ class Game:
         self.installdir = installdir
         self.sizeEstimate = sizeEstimate
         self.size = size
-    
+
     def __repr__(self):
         size = self.size or self.sizeEstimate or 0
         if size:
@@ -194,48 +208,50 @@ class Game:
             size = 'Shortcut'
         return '<Game {!r} ({})>'.format(
             self.name, size)
-    
+
     @classmethod
     def fromACF(cls, path):
         '''Game, given path to ACF reference.'''
         info = readSteamFile(path)
         if 'appid' not in info:
             raise SteamFileError('Invalid acf reference')
-        
+
         folder = info.get('installdir') or None
         installdir = None
         if folder:
             installdir = Path(os.path.split(path)[0]) / 'common' / folder
-        
+
         ID = info.get('appid')
-        name = info.get('name') or folder        
+        name = info.get('name') or folder
         sizeEstimate = int(info.get('SizeOnDisk', 0))
-        
+
         return cls(ID, name, installdir, sizeEstimate, size=None)
-    
+
     def getSize(self):
         if self.size is None:
             self.size = dirsize(self.installdir).totalSize
 
+
 class Drive:
-    __slots__ = ['path', 'games', 'sizeTotal', 'sizeUsed', 'sizeFree', 'sizeGames']
-    
+    __slots__ = ['path', 'games', 'sizeTotal',
+                 'sizeUsed', 'sizeFree', 'sizeGames']
+
     def __repr__(self):
         return '<Library {!r} ({} games, {}{})>'.format(
             self.path, len(self.games),
             "~" * any(game.size is None for game in self.games),
-            bytesize(sum(game.size or game.sizeEstimate for game in self.games)
-                     , 2, binary=True))
-    
+            bytesize(sum(game.size or game.sizeEstimate for game in self.games), 2, binary=True))
+
     def __init__(self, path):
         p = Path(path)
         self.path = p.drive or p.root
         self.games = []
-        self.sizeTotal, self.sizeUsed, self.sizeFree = shutil.disk_usage(str(path))
+        self.sizeTotal, self.sizeUsed, self.sizeFree = shutil.disk_usage(
+            str(path))
         self.sizeGames = 0
-    
+
     def appendLibrary(self, path, log=lambda *a: None):
-        '''List of games, given path to library (NOT /steamapps).'''        
+        '''List of games, given path to library (NOT /steamapps).'''
         gamespath = Path(path) / 'steamapps'
         for i in os.listdir(gamespath):
             if i.startswith('appmanifest_') and i.endswith('.acf'):
@@ -244,12 +260,13 @@ class Drive:
                     self.games.append(game)
                 except SteamFileError:
                     log('Invalid file ' + i)
-    
+
     def getSize(self):
         for game in self.games:
             game.getSize()
         self.games.sort(key=lambda g: g.size, reverse=True)
         self.sizeGames = sum(game.size for game in self.games)
+
 
 def shortcutGames(fp):
     '''Obtain games linked with shortcuts.'''
@@ -262,7 +279,7 @@ def shortcutGames(fp):
         char = file.read(1)
         if not char:
             break
-        
+
         if char == b'\0':
             if txt:
                 try:
@@ -272,9 +289,9 @@ def shortcutGames(fp):
             txt = b''
         else:
             txt += char
-    
+
     file.close()
-    
+
     games = []
     game = {}
     for i, t in enumerate(tokens):
@@ -282,7 +299,7 @@ def shortcutGames(fp):
             if game:
                 games.append(Game(**game))
             game = {}
-        
+
         elif t == '\1appname':
             game['name'] = tokens[i+1]
         elif t == '\1StartDir':
